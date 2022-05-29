1dc607809304799f9db80283add9df5981dd44cc
Ciara Thyne <ciara.thyne@gmail.com>
2022-05-28 21:07:54 -0700

    first commit

---
+++ contact.js
@@ -0,0 +1,28 @@
+// const btn = document.querySelector('button')
+// const inputs = document.querySelector('form')
+// btn.addEventListener('click', () => {
+
+    // const name = (inputs.elements["name"].value).trim()
+    // const email = (inputs.elements["email"].value).trim()
+    // const msg = (inputs.elements["message"].value.trim())
+    // const phone = (inputs.elements["phone"].value).trim()
+    // if (!name.length > 0 || !email.length > 0 || !msg.length > 0 || !phone.length > 0) {
+    //     alert("All fields are mandatory")
+    //     return
+    // }
+
+  
+
+// function sendEmail(){
+//     Email.send({
+//         Host : "smtp.mailtrap.io",
+//         Username : "388677995ecedb",
+//         Password : "e9503d9762b3e9",
+//         To : 'them@website.com',
+//         From : "you@isp.com",
+//         Subject : "This is the subject",
+//         Body : "And this is the body"
+//     }).then(
+//       message => alert(message)
+//     );
+// }
